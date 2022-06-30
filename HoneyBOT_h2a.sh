839057d9d1894fe024ed131b3ab8d021492d5870
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2022-06-30 13:16:46 -0700

    add csv and txt formats

--- HoneyBOT_h2a.sh
+++ HoneyBOT_h2a.sh
@@ -4,22 +4,30 @@ input="./HoneyBOT_Packet_Data.txt"
 ######################################
 # $IFS removed to allow the trimming #
 #####################################
+COUNTER=1
+NEW_LINE_COUNTS=`wc -l ${input}`
 while read -r line
 do
   case $line in
     "47")
-      echo "\n" # echo new line
+      echo $COUNTER,"\n" # echo new line
+      let COUNTER=COUNTER+1
     ;;
     "SYN")
-      echo "SYN"
+      echo $COUNTER,"SYN"
+      let COUNTER=COUNTER+1
     ;;
     "ACK")
-      echo "ACK"
+      echo $COUNTER,"ACK"
+      let COUNTER=COUNTER+1
     ;;
     "FIN")
-      echo "FIN"
+      echo $COUNTER,"FIN"
+      let COUNTER=COUNTER+1
     ;;
     *)
+      echo -n $COUNTER,
+      let COUNTER=COUNTER+1
       echo "$line" | xxd -r -p
   esac
 
